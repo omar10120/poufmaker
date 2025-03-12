0dfe752780c93dc2109bcb8aa7a720e2720ba085
tamer tamer <amr.dawoodi@hotmail.com>
2025-03-12 22:47:40 +0300

    modify deploy

--- next.config.ts
+++ next.config.ts
@@ -8,12 +8,23 @@ const nextConfig: NextConfig = {
       allowedOrigins: ['*']
     }
   },
-  typescript: {
-    ignoreBuildErrors: true
-  },
+  
   eslint: {
-    ignoreDuringBuilds: true, // ✅ Disable ESLint in production build
+    ignoreDuringBuilds: true, // Disable ESLint in production build
   },
+
+  typescript: {
+    ignoreBuildErrors: true, // Disable TypeScript errors during production build
+  },
+
+  // Disable CSS minification and optimization
+  optimizeFonts: false,
+  swcMinify: false,
+  
+  webpack: (config) => {
+    config.optimization.minimize = false;
+    return config;
+  }
 };
 
 export default nextConfig;
