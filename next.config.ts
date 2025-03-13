62d526a46b8563047b087d74308a45c6e80d298b
tamer tamer <amr.dawoodi@hotmail.com>
2025-03-13 04:54:03 +0300

    edit again

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    typedRoutes: false,
-    serverActions: {
-      bodySizeLimit: '2mb',
-      allowedOrigins: ['*']
-    }
+    serverActions: true
   },
   
   eslint: {
