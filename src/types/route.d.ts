2e38e110ad2aa41424a3e8418c288a35b481afa2
tamer tamer <amr.dawoodi@hotmail.com>
2025-03-13 14:13:53 +0300

    fix final

--- src/types/route.d.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export type RouteHandler<T = void> = (
-  req: NextRequest,
-  context: { params: T }
-) => Promise<NextResponse> | NextResponse;
-
-export type RouteHandlerWithId = RouteHandler<{ id: string }>;
